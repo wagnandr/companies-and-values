2b460f1ba7c14227c38b64fe20cc45683d305b3a
wagnandr <wagnandr@in.tum.de>
2016-02-27 19:11:55 +0100

    prepares deployment to heroku

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('bower_components'));
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(app.get('port'), function () {
+  console.log('Listening on port ' + app.get('port') + '!');
 });
